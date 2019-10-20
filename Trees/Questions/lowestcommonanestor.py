7d3947cbafc5553ba497f4f4351f8e9a4fabc597
Sanjay <sivasanjay7@gmail.com>
2019-10-20 22:35:20 +0530

    Eze Interview Questions

---
+++ Trees/Questions/lowestcommonanestor.py
@@ -0,0 +1,7 @@
+# Given a binary tree (not a binary search tree) and two values say n1 and n2, write a program to find the least common ancestor.
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.left=None 
+        self.right=None 
+
